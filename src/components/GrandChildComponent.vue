d7ffe5fe823545b1f11a2fcd450109818eb7946d
paturr <fathurazrahman016@gmail.com>
2024-05-17 20:10:17 +0700

    tugas4 prak pbk

---
+++ src/components/GrandChildComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="grandchild-container">
+      <h3>GrandChild Component</h3>
+      <button class="send-button" @click="sendData">Send Data to Parent</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      sendData() {
+        this.$emit('grandChildData', 'Data from GrandChild');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+.grandchild-container h3{
+    color: rgb(14, 13, 13);
+}
+
+  .grandchild-container {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+    margin-bottom: 10px;
+    text-align: center;
+    background: transparent;
+    backdrop-filter: blur(12px);
+  }
+  
+  .send-button {
+    padding: 10px 20px;
+    border: none;
+    background-color: #007bff;
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  
+  .send-button:hover {
+    background-color: #0056b3;
+  }
+  </style>
